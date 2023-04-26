2a5aaf48f5e60fee89c6ea4f9a455c4f0982fd4b
Nico Rithner <nicorithner@gmail.com>
2023-04-26 16:16:53 -0600

    Add function that returns a string to parameters

--- module-1-intro-to-java/sectionFour/Parameters.java
+++ module-1-intro-to-java/sectionFour/Parameters.java
@@ -5,13 +5,39 @@ public class Parameters {
   public static void main(String[] args) {
     BigDecimal area1 = calculateArea(2.3, 3.5);
     BigDecimal area2 = calculateArea(1.6, 2.4);
+    // BigDecimal area3 = calculateArea(-1.6, 2.4);
     System.out.println(area1);
     System.out.println(area2);
+
+    String explanation1 = explainArea(2.3, 3.5, "Spanish");
+    System.out.println(explanation1);
   }
 
   public static BigDecimal calculateArea(double length, double width) {
+    if (length < 0 || width < 0) {
+      System.out.println("INVALID DIMENSIONS");
+      System.exit(0);
+    }
+
     BigDecimal area;
     area = new BigDecimal(length * width).setScale(2, RoundingMode.HALF_EVEN);
     return area;
   }
+
+  public static String explainArea(double length, double width, String language) {
+    BigDecimal area = calculateArea(length, width);
+
+    switch (language) {
+      case "English":
+        return "Area equals length: " + length + ", * width: " + width + ". Area = " + area;
+      case "French":
+        return "La surface est egale a la longueur: " + length + ", * la largeur: " + width + ". Surface = "
+            + area;
+      case "Spanish":
+        return "Area es igual a largo: " + length + ", * ancho: " + width + ". Area = " + area;
+      default:
+        return "Language not available";
+    }
+  }
+
 }
