a72ae7805d2b3f085c13126917cc1d5aed529085
Nico Rithner <nicorithner@gmail.com>
2023-06-08 09:04:29 -0600

    Complete loop over 2d array lesson

--- module-1-intro-to-java/sectionSix/TDarrays.java
+++ module-1-intro-to-java/sectionSix/TDarrays.java
@@ -14,5 +14,31 @@ public class TDarrays {
         System.out.println("\tHarry: " + Arrays.toString(grades[0]));
         System.out.println("\tRon: " + Arrays.toString(grades[1]));
         System.out.println("\tHermione: " + Arrays.toString(grades[2]));
+
+        for (int i = 0; i < grades.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.print("\tHarry: ");
+                    break;
+                case 1:
+                    System.out.print("\tRon: ");
+                    break;
+                case 2:
+                    System.out.print("\tHermione: ");
+                    break;
+            }
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        // using for each
+        for (int[] grade : grades) {
+            for (int i : grade) {
+                System.out.print(i + " ");
+            }
+            System.out.println("\n");
+        }
     }
 }
