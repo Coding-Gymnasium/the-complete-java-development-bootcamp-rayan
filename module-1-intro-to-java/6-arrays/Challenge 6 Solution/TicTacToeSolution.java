7cda7c3e104a6c59bd8262ab9c46a7dc8c855c89
Nico Rithner <nicorithner@gmail.com>
2023-06-09 10:02:11 -0600

    Complete module 1

--- module-1-intro-to-java/6-arrays/Challenge 6 Solution/TicTacToeSolution.java
+++ module-1-intro-to-java/6-arrays/Challenge 6 Solution/TicTacToeSolution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TicTacToe {
+public class TicTacToeSolution {
 
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
