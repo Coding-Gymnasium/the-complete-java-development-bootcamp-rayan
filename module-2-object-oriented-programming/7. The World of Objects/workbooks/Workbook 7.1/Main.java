5707f74d8cee2bfc27b6d3037e3e4e0fade72762
Nico Rithner <nicorithner@gmail.com>
2023-06-09 18:21:13 -0600

    Add private fields, getters and setters

--- module-2-object-oriented-programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ module-2-object-oriented-programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -4,30 +4,30 @@ public class Main {
   public static void main(String[] args) {
     Person person = new Person();
 
-    person.name = "Rayan Slim";
-    person.nationality = "Canadian";
-    person.dateOfBirth = "01/01/1111";
-    person.passport = new String[] { person.name, person.nationality, person.dateOfBirth };
-    person.seatNumber = 5;
+    person.setName("Rayan Slim");
+    person.setNationality( "Canadian" );
+    person.setDateOfBirth( "01/01/1111" );
+    person.setName(Arrays.toString(new String[] { person.getName(), person.getNationality(), person.getDateOfBirth() }));
+    person.setSeatNumber(  5 );
 
-    System.out.println(person.name);
-    System.out.println(person.nationality);
-    System.out.println(person.dateOfBirth);
-    System.out.println(Arrays.toString(person.passport));
-    System.out.println(person.seatNumber);
+    System.out.println(person.getName());
+    System.out.println(person.getNationality());
+    System.out.println(person.getDateOfBirth());
+    System.out.println(Arrays.toString(person.getPassport()));
+    System.out.println(person.getSeatNumber());
 
     Person person1 = new Person();
-    System.out.println(person1.name);
-    System.out.println(person1.nationality);
-    System.out.println(person1.dateOfBirth);
-    System.out.println(Arrays.toString(person1.passport));
-    System.out.println(person1.seatNumber);
+    System.out.println(person1.getName());
+    System.out.println(person1.getNationality());
+    System.out.println(person1.getDateOfBirth());
+    System.out.println(Arrays.toString(person1.getPassport()));
+    System.out.println(person1.getSeatNumber());
 
     Person person2 = new Person("Liz", "USA", "09/04/1975", 1);
-    System.out.println(person2.name);
-    System.out.println(person2.nationality);
-    System.out.println(person2.dateOfBirth);
-    System.out.println(Arrays.toString(person2.passport));
-    System.out.println(person2.seatNumber);
+    System.out.println(person2.getName());
+    System.out.println(person2.getNationality());
+    System.out.println(person2.getDateOfBirth());
+    System.out.println(Arrays.toString(person2.getPassport()));
+    System.out.println(person2.getSeatNumber());
   }
 }
--- module-2-object-oriented-programming/7. The World of Objects/workbooks/Workbook 7.1/Person.java
+++ module-2-object-oriented-programming/7. The World of Objects/workbooks/Workbook 7.1/Person.java
@@ -1,9 +1,9 @@
 public class Person {
-  String name = "Nico";
-  String nationality = "USA";
-  String dateOfBirth = "07/08/1972";
-  String[] passport = { name, nationality, dateOfBirth };
-  int seatNumber = 10;
+  private String name = "Nico";
+  private String nationality = "USA";
+  private String dateOfBirth = "07/08/1972";
+  private String[] passport = { name, nationality, dateOfBirth };
+  private int seatNumber = 10;
 
   public Person() {
   };
@@ -14,5 +14,45 @@ public class Person {
     this.dateOfBirth = dateOfBirth;
     this.seatNumber = seatNumber;
     this.passport = new String[] { this.name, this.nationality, this.dateOfBirth };
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getNationality() {
+    return nationality;
+  }
+
+  public void setNationality(String nationality) {
+    this.nationality = nationality;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String[] getPassport() {
+    return passport;
+  }
+
+  public void setPassport(String[] passport) {
+    this.passport = passport;
+  }
+
+  public int getSeatNumber() {
+    return seatNumber;
+  }
+
+  public void setSeatNumber(int seatNumber) {
+    this.seatNumber = seatNumber;
   }
 }
