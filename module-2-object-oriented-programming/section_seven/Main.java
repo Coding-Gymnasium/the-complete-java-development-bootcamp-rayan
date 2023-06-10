6622504208d958b36438d6113347cd86647f2bf2
Nico Rithner <nicorithner@gmail.com>
2023-06-09 19:12:44 -0600

    Add constructor, getters and setters

--- module-2-object-oriented-programming/section_seven/Main.java
+++ module-2-object-oriented-programming/section_seven/Main.java
@@ -1,22 +1,16 @@
 package section_seven;
 
+import java.util.logging.Logger;
+
 public class Main {
   public static void main(String[] args) {
-    Car nissan = new Car();
-    nissan.make = "Nissan";
-    nissan.price = 10000;
-    nissan.year = 2020;
-    nissan.color = "Green";
+    Car nissan = new Car("Nissan", 10000, 2020, "Green");
 
-    Car dodge = new Car();
-    dodge.make = "Dodge";
-    dodge.price = 11000;
-    dodge.year = 2019;
-    dodge.color = "Blue";
+    Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
-    System.out.println("This " + nissan.make + " is worth $" + nissan.price +
-        ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-    System.out.println("This " + dodge.make + " is worth $" + dodge.price +
-        ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+    System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
+        ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+    System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
+        ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
   }
 }
