7bdc475844998097fd5df24abbe9c453ce9840d5
Nico Rithner <nicorithner@gmail.com>
2023-06-14 07:58:35 -0600

    Copy constructor lesson

--- module-2-object-oriented-programming/section_seven/Main.java
+++ module-2-object-oriented-programming/section_seven/Main.java
@@ -1,20 +1,21 @@
 package section_seven;
 
-import java.util.logging.Logger;
-
 public class Main {
   public static void main(String[] args) {
     Car nissan = new Car("Nissan", 10000, 2020, "Green");
+    Car nissan2 = new Car(nissan); // this just point to the original object.
 
-    Car dodge = new Car("Dodge", 11000, 2019, "Blue");
-
-    nissan.setColor("Jet Black");
-    dodge.setPrice(5000);
-    nissan.setPrice(nissan.getPrice() / 3);
+    nissan2.setColor("Yellow");
+    nissan.setColor("Orange");
+    nissan2.setColor("Blue");
+    nissan.setColor("Purple");
+    nissan2.setColor("Fuchsia");
+    nissan.setColor("Beige");
 
     System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
         ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
-    System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
-        ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+
+    System.out.println("This " + nissan2.getMake() + " is worth $" + nissan2.getPrice() +
+        ". It was built in " + nissan2.getYear() + ". It is " + nissan2.getColor() + ".\n");
   }
 }
