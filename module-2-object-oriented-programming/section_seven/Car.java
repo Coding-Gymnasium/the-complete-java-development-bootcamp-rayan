cf979e3d2cf7df72cfc27de6cfbcfc3a851a7e0d
Nico Rithner <nicorithner@gmail.com>
2023-06-20 18:26:54 -0600

    Add copy constructor

--- module-2-object-oriented-programming/section_seven/Car.java
+++ module-2-object-oriented-programming/section_seven/Car.java
@@ -10,7 +10,7 @@ public class Car {
   private String[] parts;
 
   public Car(String make, double price, int year, String color,
-             String[] parts) {
+      String[] parts) {
     this.make = make;
     this.price = price;
     this.year = year;
@@ -23,33 +23,53 @@ public class Car {
     this.price = source.price;
     this.year = source.year;
     this.color = source.color;
-    this.parts = source.parts;
+    this.parts = Arrays.copyOf(source.parts, source.parts.length);
   }
 
-  public String getMake() { return this.make; }
+  public String getMake() {
+    return this.make;
+  }
 
-  public void setMake(String make) { this.make = make; }
+  public void setMake(String make) {
+    this.make = make;
+  }
 
-  public double getPrice() { return this.price; }
+  public double getPrice() {
+    return this.price;
+  }
 
-  public void setPrice(double price) { this.price = price; }
+  public void setPrice(double price) {
+    this.price = price;
+  }
 
-  public int getYear() { return this.year; }
+  public int getYear() {
+    return this.year;
+  }
 
-  public void setYear(int year) { this.year = year; }
+  public void setYear(int year) {
+    this.year = year;
+  }
 
-  public String getColor() { return this.color; }
+  public String getColor() {
+    return this.color;
+  }
 
-  public void setColor(String color) { this.color = color; }
+  public void setColor(String color) {
+    this.color = color;
+  }
 
-  public String[] getParts() { return this.parts; }
+  public String[] getParts() {
+    return this.parts;
+  }
 
-  public void setParts(String[] parts) { this.parts = parts; }
+  public void setParts(String[] parts) {
+    this.parts = parts;
+  }
 
   public void drive() {
     System.out.println("\nYou bought the beautiful " + this.year + " " +
-                       this.color + " " + this.make + " for " + this.price +
-                       " 🎉.");
+        this.color + " " + this.make + " for " + this.price +
+        " 🎉.");
     System.out.println(this.parts[0]);
   }
 }
--- module-2-object-oriented-programming/section_seven/Main.java
+++ module-2-object-oriented-programming/section_seven/Main.java
@@ -2,14 +2,12 @@ package section_seven;
 
 public class Main {
   public static void main(String[] args) {
-    String[] spareParts = new String[] {"Tires", "Keys"};
-    Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
-    Car nissan2 = new Car(nissan); // this just point to the original object.
-    
-    spareParts[0] = "Filter";
+
+    Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] { "Tires", "Keys" });
+    Car nissan2 = new Car(nissan);
 
     nissan2.setColor("Yellow");
-
+    nissan.drive();
     nissan2.drive();
   }
 }
