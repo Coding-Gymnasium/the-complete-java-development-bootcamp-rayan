cb94c6713bf26caf535dc316bd6fafa33e3ac020
Nico Rithner <nicorithner@gmail.com>
2023-06-20 19:08:27 -0600

    Fix setParts setter

--- module-2-object-oriented-programming/section_seven/Main.java
+++ module-2-object-oriented-programming/section_seven/Main.java
@@ -4,12 +4,13 @@ public class Main {
   public static void main(String[] args) {
 
     Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] { "Tires", "Keys" });
-    String[] carParts = nissan.getParts();
-    carParts[0] = "Filter"; // this should have no effect on carParts if we create a copy of the array
-                            // this.parts in the class
     nissan.drive();
 
     Car nissan2 = new Car(nissan);
+    String[] carParts = nissan.getParts();
+    carParts[0] = "Filter";
+
+    nissan2.setParts(carParts);
     nissan2.setColor("Yellow");
     nissan2.drive();
   }
